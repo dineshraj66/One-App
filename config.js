478411c0816b66a3e72692a4b8eb207a90479341
dineshraj66 <88567787+dineshraj66@users.noreply.github.com>
2026-03-08 15:33:25 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -23,59 +23,71 @@ const DASHBOARD_CONFIG = {
             }
         },
         pageTurn: {
-            url: 'https://dineshraj66.github.io/PageTurn/',
-            firebase: {
-                apiKey: "YOUR_PAGETURN_API_KEY",
-                projectId: "pageturn-project-id",
-                databaseURL: "https://pageturn-db-url"
-            }
+        url: 'https://dineshraj66.github.io/PageTurn/',
+        firebase: {
+        apiKey: "AIzaSyD-2SCMUJsrkmmWo4IUmWpjPu5TC99C-ho",
+        projectId: "book-tracker-5bc5f",
+        authDomain: "book-tracker-5bc5f.firebaseapp.com",
+        storageBucket: "book-tracker-5bc5f.firebasestorage.app",
+        messagingSenderId: "689545850122",
+        appId: "1:689545850122:web:f0d244bcf9a4c9e2c51ef3"
+        }
         },
         dailyReminder: {
             url: 'https://dineshraj66.github.io/DailyReminder/',
             firebase: {
-                apiKey: "YOUR_DAILYREMINDER_API_KEY",
-                projectId: "dailyreminder-project-id",
-                databaseURL: "https://dailyreminder-db-url"
+                apiKey: "AIzaSyAAfFKsEwc-ZrKmtnKaCOMIB03es0dRu38",
+                projectId: "taskreminderapp-28eeb",
+                databaseURL: "https://taskreminderapp-28eeb-default-rtdb.asia-southeast1.firebasedatabase.app"
             }
         },
         lifeLog: {
-            url: 'https://dineshraj66.github.io/LifeLog/',
-            firebase: {
-                apiKey: "YOUR_LIFELOG_API_KEY",
-                projectId: "lifelog-project-id",
-                databaseURL: "https://lifelog-db-url"
-            }
+        url: 'https://dineshraj66.github.io/LifeLog/',
+        firebase: {
+        apiKey: "AIzaSyCXK_X1e_K_-Wz-gE_9g6dm96vMLdKViq4",
+        projectId: "daily-events-a3151",
+        authDomain: "daily-events-a3151.firebaseapp.com",
+        storageBucket: "daily-events-a3151.firebasestorage.app",
+        messagingSenderId: "744894479261",
+        appId: "1:744894479261:web:ae30525105eb58830f8d74"
+        }
         },
         tradeTracker: {
-            url: 'https://dineshraj66.github.io/TradeTracker/',
-            firebase: {
-                apiKey: "YOUR_TRADETRACKER_API_KEY",
-                projectId: "tradetracker-project-id",
-                databaseURL: "https://tradetracker-db-url"
-            }
+        url: 'https://dineshraj66.github.io/TradeTracker/',
+        firebase: {
+        apiKey: "AIzaSyCr5O3rDc-UfazAwnqTS8KOEvn22ksHu3U",
+        projectId: "trade-tracker-71027",
+        authDomain: "trade-tracker-71027.firebaseapp.com",
+        storageBucket: "trade-tracker-71027.firebasestorage.app",
+        messagingSenderId: "702315259190",
+        appId: "1:702315259190:web:e2716abba4080558dc3f97"
+        }
         },
         loveToday: {
-            url: 'https://dineshraj66.github.io/LoveToday/',
-            firebase: {
-                apiKey: "YOUR_LOVETODAY_API_KEY",
-                projectId: "lovetoday-project-id",
-                databaseURL: "https://lovetoday-db-url"
-            }
+        url: 'https://dineshraj66.github.io/LoveToday/',
+        firebase: {
+        apiKey: "AIzaSyAtmbfK7tKPTioxoQuC0kqq9Y0MGC95wPE",
+        projectId: "love-today-231dc",
+        authDomain: "love-today-231dc.firebaseapp.com",
+        storageBucket: "love-today-231dc.firebasestorage.app",
+        messagingSenderId: "576243792568",
+        appId: "1:576243792568:web:1e4acfb0c4795dc318524d"
+        }
         },
         cashLedger: {
             url: 'https://dineshraj66.github.io/CashLedger/',
             firebase: {
-                apiKey: "YOUR_CASHLEDGER_API_KEY",
+                apiKey: "AIzaSyD8R4KazYZRrms8PBb6Ok7UI5XQQed9v8s",
                 projectId: "cashledger-project-id",
-                databaseURL: "https://cashledger-db-url"
+                databaseURL: "https://cash-ledger-app-b4f57-default-rtdb.asia-southeast1.firebasedatabase.app"
             }
         },
         angryBird: {
             url: 'https://dineshraj66.github.io/AngryBird/',
             firebase: {
-                apiKey: "YOUR_angrybird_API_KEY",
-                projectId: "angrybird-project-id",
-                databaseURL: "https://angrybird-db-url"
+                apiKey: "AIzaSyAtgu0H9O1pj3abAOHUpM6qdrIGzw9WRYc",
+                projectId: "angry-bird-2776f",
+                databaseURL: "https://angry-bird-2776f-default-rtdb.asia-southeast1.firebasedatabase.app"
             }
         }
     }
