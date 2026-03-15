5b2d4b6a3cc24fab238205750cb7ee42e3f55e78
dineshraj66 <88567787+dineshraj66@users.noreply.github.com>
2026-03-15 13:10:30 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -23,15 +23,15 @@ const DASHBOARD_CONFIG = {
             }
         },
         pageTurn: {
-        url: 'https://dineshraj66.github.io/Page_Turn/',
-        firebase: {
-        apiKey: "AIzaSyD-2SCMUJsrkmmWo4IUmWpjPu5TC99C-ho",
-        projectId: "book-tracker-5bc5f",
-        authDomain: "book-tracker-5bc5f.firebaseapp.com",
-        storageBucket: "book-tracker-5bc5f.firebasestorage.app",
-        messagingSenderId: "689545850122",
-        appId: "1:689545850122:web:f0d244bcf9a4c9e2c51ef3"
-        }
+            url: 'https://dineshraj66.github.io/Page_Turn/',
+            firebase: {
+                apiKey: "AIzaSyD-2SCMUJsrkmmWo4IUmWpjPu5TC99C-ho",
+                projectId: "book-tracker-5bc5f",
+                authDomain: "book-tracker-5bc5f.firebaseapp.com",
+                storageBucket: "book-tracker-5bc5f.firebasestorage.app",
+                messagingSenderId: "689545850122",
+                appId: "1:689545850122:web:f0d244bcf9a4c9e2c51ef3"
+            }
         },
         dailyReminder: {
             url: 'https://dineshraj66.github.io/Daily-Reminder-App/',
@@ -42,37 +42,37 @@ const DASHBOARD_CONFIG = {
             }
         },
         lifeLog: {
-        url: 'https://dineshraj66.github.io/Life_Log/',
-        firebase: {
-        apiKey: "AIzaSyCXK_X1e_K_-Wz-gE_9g6dm96vMLdKViq4",
-        projectId: "daily-events-a3151",
-        authDomain: "daily-events-a3151.firebaseapp.com",
-        storageBucket: "daily-events-a3151.firebasestorage.app",
-        messagingSenderId: "744894479261",
-        appId: "1:744894479261:web:ae30525105eb58830f8d74"
-        }
+            url: 'https://dineshraj66.github.io/Life_Log/',
+            firebase: {
+                apiKey: "AIzaSyCXK_X1e_K_-Wz-gE_9g6dm96vMLdKViq4",
+                projectId: "daily-events-a3151",
+                authDomain: "daily-events-a3151.firebaseapp.com",
+                storageBucket: "daily-events-a3151.firebasestorage.app",
+                messagingSenderId: "744894479261",
+                appId: "1:744894479261:web:ae30525105eb58830f8d74"
+            }
         },
         tradeTracker: {
-        url: 'https://dineshraj66.github.io/Trade-Tracker/',
-        firebase: {
-        apiKey: "AIzaSyCr5O3rDc-UfazAwnqTS8KOEvn22ksHu3U",
-        projectId: "trade-tracker-71027",
-        authDomain: "trade-tracker-71027.firebaseapp.com",
-        storageBucket: "trade-tracker-71027.firebasestorage.app",
-        messagingSenderId: "702315259190",
-        appId: "1:702315259190:web:e2716abba4080558dc3f97"
-        }
+            url: 'https://dineshraj66.github.io/Trade-Tracker/',
+            firebase: {
+                apiKey: "AIzaSyCr5O3rDc-UfazAwnqTS8KOEvn22ksHu3U",
+                projectId: "trade-tracker-71027",
+                authDomain: "trade-tracker-71027.firebaseapp.com",
+                storageBucket: "trade-tracker-71027.firebasestorage.app",
+                messagingSenderId: "702315259190",
+                appId: "1:702315259190:web:e2716abba4080558dc3f97"
+            }
         },
         loveToday: {
-        url: 'https://dineshraj66.github.io/Love-Today/',
-        firebase: {
-        apiKey: "AIzaSyAtmbfK7tKPTioxoQuC0kqq9Y0MGC95wPE",
-        projectId: "love-today-231dc",
-        authDomain: "love-today-231dc.firebaseapp.com",
-        storageBucket: "love-today-231dc.firebasestorage.app",
-        messagingSenderId: "576243792568",
-        appId: "1:576243792568:web:1e4acfb0c4795dc318524d"
-        }
+            url: 'https://dineshraj66.github.io/Love-Today/',
+            firebase: {
+                apiKey: "AIzaSyAtmbfK7tKPTioxoQuC0kqq9Y0MGC95wPE",
+                projectId: "love-today-231dc",
+                authDomain: "love-today-231dc.firebaseapp.com",
+                storageBucket: "love-today-231dc.firebasestorage.app",
+                messagingSenderId: "576243792568",
+                appId: "1:576243792568:web:1e4acfb0c4795dc318524d"
+            }
         },
         cashLedger: {
             url: 'https://dineshraj66.github.io/Cash-Ledger/',
